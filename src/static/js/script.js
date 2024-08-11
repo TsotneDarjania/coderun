0ffb85984a53c97a34641a8c6d4354744be7f4ee
Tsotne Darjania <98889556+TsotneDarjania@users.noreply.github.com>
2024-08-11 15:46:48 +0400

    create home page

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -4,4 +4,13 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function init() {
   console.log("Full Stack Web Boilerplate");
+
+  window.addEventListener("scroll", () => {
+    const header = document.getElementById("nav");
+    const navTexts = document.querySelectorAll(".nav-text");
+    header.classList.toggle("below-nav", window.scrollY > 0);
+    navTexts.forEach((navText) => {
+      navText.classList.toggle("nav-text-below", window.scrollY > 0);
+    });
+  });
 }
